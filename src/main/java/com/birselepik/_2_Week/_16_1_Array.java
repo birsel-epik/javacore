1614dab3d6d886cee9ccc96fc38e12acb2b1f402
birsel <birsel.epik@gmail.com>
2025-02-18 13:14:30 +0300

    feat(javacore): Array

--- src/main/java/com/birselepik/_2_Week/_16_1_Array.java
+++ src/main/java/com/birselepik/_2_Week/_16_1_Array.java
@@ -4,7 +4,7 @@ public class _16_1_Array {
 
     // Array
     public static String[] arrayMethod1() throws ArrayIndexOutOfBoundsException { // istisnasi durum yaşanabilir, elemenlardan biri eksik olabilir
-        // String dizisi.
+        // String Dizisi
         // Array da başlangıç değeri veriliyor. (Collection lar da başlangıç değeri verilmez.)
         String[] city = new String[6]; // Eleman sayısı: 5 // diziler 0 dan başlayarak saymaya başlar // Boş data olunca null ekliyor
         city[0] = "İstanbul";
@@ -22,13 +22,27 @@ public class _16_1_Array {
         return city; // String diziyi döndürüyoruz
     }
 
-    public static void arrayMethod2() {
+    // Array
+    // 2. YÖNTEM
+    public static String[] arrayMethod2() throws ArrayIndexOutOfBoundsException {
+        String[] city = {"Malatya", "İstanbul", "Malatya", "Elazığ", "İzmir", "Ankara"}; // içinde olmayan data yoksa, null yazmak zorunda değiliz yazmasakta çalışır
+
+        System.out.println(city);
+        System.out.println("Eleman Sayısı: " + city.length); // Dizinin eleman sayısı
+        System.out.println(city[0]);
+        System.out.println("Son Eleman: " + city[6 - 1]);
+        System.out.println("Son Eleman: " + city[(city.length) - 1]);
+
+        return city; // String diziyi döndürüyoruz
+    }
+
+    public static void arrayMethod3() {
         String[] city = arrayMethod1(); // yukarıdaki String dizisini çağırıyoruz
     }
 
     // for each
-    public static void arrayMethod3() {
-        String[] city = arrayMethod1(); // yukarıdaki String dizisini çağırıyoruz
+    public static void arrayMethod4() {
+        String[] city = arrayMethod2(); // yukarıdaki String dizisini çağırıyoruz
 
         // for each döngüsü
         for (String temp : city) { // temp = Geçici data
@@ -36,8 +50,8 @@ public class _16_1_Array {
         }
 
         System.out.println();
-        
-        // Renklendirilmiş versiyonu
+
+        // Döngünün renklendirilmiş versiyonu
         for (String temp : city) { // temp = Geçici data
             System.out.println(_15_4_Special_Color.BLUE + temp + _15_4_Special_Color.RESET);
         }
@@ -48,6 +62,7 @@ public class _16_1_Array {
     public static void main(String[] args) {
         // arrayMethod1();
         // arrayMethod2();
-        arrayMethod3();
+        // arrayMethod3();
+        arrayMethod4();
     }
 } // end class
