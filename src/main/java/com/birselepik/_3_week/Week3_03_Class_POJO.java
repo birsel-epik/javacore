35fd0722ce8f1da45feb5863bd66d5f3c3ff3066
birsel <birsel.epik@gmail.com>
2025-02-21 21:10:24 +0300

    feat(javacore): POJO: Plain Old Java Object (fields + getter and setter)

--- src/main/java/com/birselepik/_3_week/Week3_03_Class_POJO.java
+++ src/main/java/com/birselepik/_3_week/Week3_03_Class_POJO.java
@@ -1,9 +1,43 @@
 package com.birselepik._3_week;
 
-import java.util.Scanner;
+// POJO: Plain Old Java Object
+// Sadece fields + getter and setter
 
+
+import com.birselepik.utils.SpecialColor;
+
+// this: global nesneyi işaret eder.
 public class Week3_03_Class_POJO {
 
+    // Field
+    private String name;
+    private String surname;
 
+
+    // Getter And Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        // name = _name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname.toUpperCase();
+    }
+
+    public static void main(String[] args) {
+        Week3_03_Class_POJO pojo = new Week3_03_Class_POJO();
+        pojo.setName("Birsel");
+        pojo.setSurname("Epik");
+        String fullName = pojo.getName().toString() + " " + pojo.getSurname().toString();
+        System.out.println(SpecialColor.BLUE + fullName + SpecialColor.RESET);
+    }
 
 }
