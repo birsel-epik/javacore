ee1082fe40d5ff5bd1bf7ec32873d5091f31c60c
birsel <birsel.epik@gmail.com>
2025-02-21 22:37:15 +0300

    feat(javacore): LOMBOK kütüphanesi

--- src/main/java/com/birselepik/_3_week/Week3_05_Class_LOMBOK.java
+++ src/main/java/com/birselepik/_3_week/Week3_05_Class_LOMBOK.java
@@ -1,4 +1,37 @@
 package com.birselepik._3_week;
 
+// Java'da POJO sınıfları genellikle getter, setter ve constructor gibi birçok tekrar eden kod içerdiğinden,
+// **Lombok** kütüphanesi POJO'ları daha az kod ile yazmayı sağlar.
+
+import lombok.*;
+
+import java.util.Date;
+
+@NoArgsConstructor // parametresiz Constructor
+@AllArgsConstructor // parametreli Constructor
+// @Data
+@Getter
+@Setter
+@ToString
+@Builder
+
 public class Week3_05_Class_LOMBOK {
+
+    // Field
+    private Long id;
+    private String name;
+    private String surname;
+    private Date createdDate;
+
+    public static void main(String[] args) {
+        Week3_05_Class_LOMBOK lombok = Week3_05_Class_LOMBOK.builder()
+                .id(1L)
+                .name("Birsel")
+                .surname("Epik")
+                .createdDate(new Date(System.currentTimeMillis()))
+                .build();
+
+        System.out.println(lombok);
+    }
+
 }
