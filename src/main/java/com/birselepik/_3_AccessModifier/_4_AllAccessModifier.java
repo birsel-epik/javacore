a79095a88d734a9c6dfbda8d0c0532406fcaf8e2
birsel <birsel.epik@gmail.com>
2025-02-22 14:51:55 +0300

    fix(javacore): file name changed

--- src/main/java/com/birselepik/_3_AccessModifier/_4_AllAccessModifier.java
+++ src/main/java/com/birselepik/_3_AccessModifier/_4_AllAccessModifier.java
@@ -1,12 +1,12 @@
 package com.birselepik._3_AccessModifier;
 
-import com.birselepik._3_week.Week3_01_Access_01_PublicAccessModifier;
+import com.birselepik._3_week.Week3_01_1_PublicAccessModifier;
 import com.birselepik.utils.SpecialColor;
 
 public class _4_AllAccessModifier {
 
     public static void main(String[] args) {
-        Week3_01_Access_01_PublicAccessModifier accessModifier = new Week3_01_Access_01_PublicAccessModifier();
+        Week3_01_1_PublicAccessModifier accessModifier = new Week3_01_1_PublicAccessModifier();
         System.out.println(SpecialColor.BLUE + accessModifier.publicData + SpecialColor.RESET);
         //System.out.println(SpecialColor.YELLOW+accessModifier.defaultData+SpecialColor.RESET);
         //System.out.println(SpecialColor.PURPLE+accessModifier.protectedData+SpecialColor.RESET);
--- src/main/java/com/birselepik/_3_week/Week3_01_1_PublicAccessModifier.java
+++ src/main/java/com/birselepik/_3_week/Week3_01_1_PublicAccessModifier.java
@@ -2,7 +2,7 @@ package com.birselepik._3_week;
 
 import com.birselepik.utils.SpecialColor;
 
-public class Week3_01_Access_01_PublicAccessModifier {
+public class Week3_01_1_PublicAccessModifier {
 
     public String publicData = "public data"; // Her yerden erişilebilir
     private String privateData = "private data"; // Sadece tanımlandığı sınıf içinden erişilebilir
@@ -10,7 +10,7 @@ public class Week3_01_Access_01_PublicAccessModifier {
     String defaultData = "default data"; // (Hiçbir şey yazılmaz) | Sadece aynı paket içinden erişilebilir
 
     public static void main(String[] args) {
-        Week3_01_Access_01_PublicAccessModifier accessModifier = new Week3_01_Access_01_PublicAccessModifier();
+        Week3_01_1_PublicAccessModifier accessModifier = new Week3_01_1_PublicAccessModifier();
         System.out.println(SpecialColor.BLUE + accessModifier.publicData + SpecialColor.RESET);
         System.out.println(SpecialColor.YELLOW + accessModifier.defaultData + SpecialColor.RESET);
         System.out.println(SpecialColor.PURPLE + accessModifier.protectedData + SpecialColor.RESET);
